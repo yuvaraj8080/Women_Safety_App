08a57d8ff24bb0f80154b6666fa05cfc072624a0
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-08-23 20:55:20 +0530

    adding url launcher to the creme

--- lib/common/NetworkManager/network_manager.dart
+++ lib/common/NetworkManager/network_manager.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -6,45 +7,41 @@ import 'package:get/get.dart';
 import '../widgets.Login_Signup/loaders/snackbar_loader.dart';
 
 
-class NetworkManager extends GetxController{
+class NetworkManager extends GetxController {
   static NetworkManager get instance => Get.find();
 
   final Connectivity _connectivity = Connectivity();
-  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
 
-  final Rx<ConnectivityResult> _connectionStatus = ConnectivityResult.none.obs;
+  final Rx<ConnectivityResult> _connectionStatus =
+      ConnectivityResult.none.obs;
 
   @override
-  void onInit(){
+  void onInit() {
     super.onInit();
-    _connectivitySubscription = _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+    _connectivitySubscription = _connectivity.onConnectivityChanged
+        .listen(_updateConnectionStatus);
   }
 
-  Future<void> _updateConnectionStatus(ConnectivityResult result)async{
+  void _updateConnectionStatus(List<ConnectivityResult> event) {
+    final result = event.isNotEmpty ? event.first : ConnectivityResult.none;
     _connectionStatus.value = result;
-    if(_connectionStatus.value == ConnectivityResult.none){
+    if (_connectionStatus.value == ConnectivityResult.none) {
       TLoaders.customToast(message: "No Internet Connection");
     }
   }
+
   Future<bool> isConnected() async {
-    try{
+    try {
       final result = await _connectivity.checkConnectivity();
-      if(result == ConnectivityResult.none){
-        return false;
-      }
-      else{
-        return true;
-      }
-    }
-    on PlatformException catch(_){
+      return result != ConnectivityResult.none;
+    } on PlatformException catch (_) {
       return false;
     }
   }
 
-  /// DISPOSE OR CLOSE THE ACTIVE CONNECTIVITY STREAM
-
   @override
-  void onClose(){
+  void onClose() {
     super.onClose();
     _connectivitySubscription.cancel();
   }
