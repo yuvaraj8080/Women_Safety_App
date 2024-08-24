29d0b52f0d2e504ec27a509507b3f195091f6a3e
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-08-24 11:22:24 +0530

    adding url launcher to the creme

--- lib/features/Advanced_Safety_Tool/Report_Incident/controller/reportIncident_controller.dart
+++ lib/features/Advanced_Safety_Tool/Report_Incident/controller/reportIncident_controller.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_women_safety_app/data/repositories/authentication/authentication-repository.dart';
-import 'package:flutter_women_safety_app/data/repositories/user/user_repository.dart';
-import 'package:flutter_women_safety_app/features/personalization/controllers/user_controller.dart';
+import 'package:flutter_women_safety_app/utils/constants/image_string.dart';
 import 'package:get/get.dart';
 
 import '../../../../common/NetworkManager/network_manager.dart';
 import '../../../../common/widgets.Login_Signup/loaders/snackbar_loader.dart';
 import '../../../../data/repositories/report_repository/report_repository.dart';
 import '../../../../utils/popups/full_screen_loader.dart';
+import '../../../personalization/controllers/user_controller.dart';
 import '../../models/ReportIncidentModel.dart';
 
 class ReportIncidentController extends GetxController {
@@ -18,18 +17,19 @@ class ReportIncidentController extends GetxController {
 
   final description = TextEditingController();
   final city = TextEditingController();
-  final liveLocation = TextEditingController();
   final type = TextEditingController();
 
   final reportRepository = Get.put(ReportIncidentRepository());
   final userController = Get.put(UserController());
 
+  List<String> types = ['Harassment', 'Rape', 'Abuse', "Others"];
+  List<String> cities = ['Mumbai', 'Pune', 'Delhi', 'Nashik'];
 
   /// SAVE REPORT INCIDENT
   Future<void> createReportIncident() async {
     try {
       /// START LOADING
-      TFullScreenLoader.openLoadingDialog("Wating For Report",'');
+      TFullScreenLoader.openLoadingDialog("Wait for reporting",TImages.loadingLottie);
 
       /// CHECK INTERNET CONNECTIVITY
       final isConnected = await NetworkManager.instance.isConnected();
@@ -50,7 +50,7 @@ class ReportIncidentController extends GetxController {
         description: description.text.trim(),
         city: city.text.trim(),
         fullName:authUser.fullName,
-        liveLocation: liveLocation.text.trim(),
+        liveLocation:"",
         phoneNo:authUser.phoneNumber,
         time:DateTime.now(),
         type:type.text.trim(),
@@ -62,23 +62,23 @@ class ReportIncidentController extends GetxController {
 
       /// RESET FIELDS
       resetFields();
+      Get.back();
 
       /// REMOVE LOADER
       TFullScreenLoader.stopLoading();
 
       /// SHOW SUCCESS MESSAGE
-      TLoaders.successSnackBar(
-          title: "Success", message: "Report incident saved successfully");
+      TLoaders.successSnackBar(title: "Success", message: "Report incident saved successfully");
     } catch (e) {
       TFullScreenLoader.stopLoading();
       TLoaders.errorSnackBar(title: "Error", message: "Failed to save report incident: $e");
+
     }
   }
 
   void resetFields() {
     description.clear();
     city.clear();
-    liveLocation.clear();
     loading(false);
   }
 }
--- lib/features/Advanced_Safety_Tool/Report_Incident/widgets/ReportForm_bottomsheet.dart
+++ lib/features/Advanced_Safety_Tool/Report_Incident/widgets/ReportForm_bottomsheet.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+
 import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/validators/validator.dart';
+import '../controller/reportIncident_controller.dart';
 
 class IncidentReportBottomSheet extends StatefulWidget {
   @override
@@ -9,11 +12,7 @@ class IncidentReportBottomSheet extends StatefulWidget {
 }
 
 class _IncidentReportBottomSheetState extends State<IncidentReportBottomSheet> {
-  String? _selectedType;
-  String? _selectedCity;
-
-  List<String> _types = ['Harassment', 'Rape', 'Abuse', "Others"];
-  List<String> _cities = ['Mumbai', 'Pune', 'Delhi', 'Nashik'];
+  final ReportIncidentController _reportIncidentController = Get.put(ReportIncidentController());
 
   @override
   Widget build(BuildContext context) {
@@ -22,71 +21,72 @@ class _IncidentReportBottomSheetState extends State<IncidentReportBottomSheet> {
       child: Padding(
         padding: const EdgeInsets.all(18),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Incident Report", style: Theme.of(context).textTheme.headlineSmall),
-              SizedBox(height: TSizes.size24),
-              TextFormField(
-                expands: false,
-                maxLines: 2,
-                decoration: const InputDecoration(
-                    labelText: "Incident description", prefixIcon: Icon(Iconsax.edit)),
-                validator: (value) => TValidator.validateEmptyText("description", value),
-              ),
-              const SizedBox(height: TSizes.size16),
-              Row(
-                children: [
-                  ///  Type Dropdown
-                  Expanded(
-                    child: DropdownButtonFormField(
-                      decoration: const InputDecoration(
+          child: Form(
+            key:_reportIncidentController.reportKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Incident Report", style: Theme.of(context).textTheme.headlineSmall),
+                SizedBox(height: TSizes.size24),
+                TextFormField(
+                  controller: _reportIncidentController.description,
+                  expands: false,
+                  maxLines: 2,
+                  decoration: const InputDecoration(
+                      labelText: "Incident description", prefixIcon: Icon(Iconsax.edit)),
+                  validator: (value) => TValidator.validateEmptyText("description", value),
+                ),
+                const SizedBox(height: TSizes.size16),
+                Row(
+                  children: [
+                    ///  Type Dropdown
+                    Expanded(
+                      child: DropdownButtonFormField(
+                        decoration: const InputDecoration(
                           labelText: "Type",),
-                      items: _types.map((type) {
-                        return DropdownMenuItem(
-                          child: Text(type),
-                          value: type,
-                        );
-                      }).toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          _selectedType = value;
-                        });
-                      },
-                      validator: (value) => TValidator.validateEmptyText("Incident Type", value),
+                        items: _reportIncidentController.types.map((type) {
+                          return DropdownMenuItem(
+                            child: Text(type),
+                            value: type,
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            _reportIncidentController.type.text = value as String;
+                          });
+                        },
+                        validator: (value) => TValidator.validateEmptyText("Incident Type", value),
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: TSizes.size8),
-                  /// City Dropdown
-                  Expanded(
-                    child: DropdownButtonFormField(
-                      decoration: const InputDecoration(
-                          labelText: "City", prefixIcon: Icon(Iconsax.building)),
-                      items: _cities.map((city) {
-                        return DropdownMenuItem(
-                          child: Text(city),
-                          value: city,
-                        );
-                      }).toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          _selectedCity = value;
-                        });
-                      },
-                      validator: (value) => TValidator.validateEmptyText("Incident City", value),
+                    const SizedBox(width: TSizes.size8),
+                    /// City Dropdown
+                    Expanded(
+                      child: DropdownButtonFormField(
+                        decoration: InputDecoration(
+                            labelText: "City", prefixIcon: Icon(Iconsax.building)),
+                        items: _reportIncidentController.cities.map((city) {
+                          return DropdownMenuItem(
+                            child: Text(city),
+                            value: city,
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            _reportIncidentController.city.text = value as String;
+                          });
+                        },
+                        validator: (value) => TValidator.validateEmptyText("Incident City", value),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-          
-              const SizedBox(height:30),
-          
-              /// Sign Up Button Create Account button
-              SizedBox(width:double.infinity,
-                  child:ElevatedButton(
-                      onPressed:(){},
-                      child:const Text("Submit Report")))
-            ],
+                  ],
+                ),
+                const SizedBox(height:30),
+                /// Sign Up Button Create Account button
+                SizedBox(width:double.infinity,
+                    child:ElevatedButton(onPressed:(){_reportIncidentController.createReportIncident();},
+                        child:const Text("Submit Report")))
+              ],
+            ),
           ),
         ),
       ),
